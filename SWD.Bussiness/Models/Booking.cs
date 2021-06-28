dd469a445bac0e6ead19c0b4cf6a7c118d5c0719
Thanh Duyên Trần Nguyễn <tranduyen07121999@gmail.com>
2021-06-28 18:24:23 +0700

    .

--- SWD.Bussiness/Models/Booking.cs
+++ SWD.Bussiness/Models/Booking.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 // If you have enabled NRTs for your project, then un-comment the following line:
 // #nullable disable
 
-namespace SWD.Bussiness.Models
+namespace SWD.Bussiness.Models 
 {
     public partial class Booking
     {
